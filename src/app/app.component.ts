6bf0fbd8f2f8b5a273cd18db897cfa99afabd323
danilodejesus <danilojesusv@gmail.com>
2020-01-31 21:07:37 -0500

    observables and operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from, of } from 'rxjs'
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -11,5 +12,23 @@ export class AppComponent implements OnInit  {
 
   ngOnInit() {
     of(2, 4, 6, 8).subscribe(console.log);
+    
+    from([20, 15, 40, 5])
+    .pipe(
+      tap(item => (console.log(`valor origingal: ${item}`))),
+      map(item => item * 2),
+      map(item => item - 10),
+      map(item => {
+        if (item === 0) {
+          throw new Error('Zero detected')
+        }
+        return item
+      }),
+      take(3)
+    ).subscribe(
+      item => console.log(`item: ${item}`),
+      err => console.log(`error occurred ${err}`),
+      () => console.log(`complete`)
+    );
   }
 }
